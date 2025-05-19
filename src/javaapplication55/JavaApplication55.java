31dcb7768f0e8ff1e9b716cd12e3d04b64478a39
IndikaMadhushan <indikamadushan797@gmail.com>
2025-05-19 22:06:27 +0530

    second

--- src/javaapplication55/JavaApplication55.java
+++ src/javaapplication55/JavaApplication55.java
@@ -14,7 +14,7 @@ public class JavaApplication55 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("first");
+        System.out.println("second");
     }
     
 }
